b8f0d25a0ce04a500cb422557952349c336853b3
zHElEARN <personal@zhelearn.com>
2025-07-07 18:18:52 +0800

    Initial Commit

---
+++ CSUSTPlanet/CSUSTPlanetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSUSTPlanetApp.swift
+//  CSUSTPlanet
+//
+//  Created by Zhe_Learn on 2025/7/7.
+//
+
+import SwiftUI
+
+@main
+struct CSUSTPlanetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
