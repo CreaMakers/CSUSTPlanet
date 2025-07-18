a745eb7962248ea3a02eb50cb2a3db33e01e8283
zHElEARN <personal@zhelearn.com>
2025-07-18 22:55:31 +0800

    feat: add campus map view with location selection and zoom

--- CSUSTPlanet/Views/Features/CampusMapView.swift
+++ CSUSTPlanet/Views/Features/CampusMapView.swift
@@ -5,14 +5,48 @@
 //  Created by Zhe_Learn on 2025/7/11.
 //
 
+import CSUSTKit
 import SwiftUI
+import Zoomable
 
 struct CampusMapView: View {
+    @State private var selectedCampus: Campus = .jinpenling
+
+    private var mapImageName: String {
+        switch selectedCampus {
+        case .yuntang: return "YuntangMap"
+        case .jinpenling: return "JinpenlingMap"
+        }
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            Picker("选择校区", selection: $selectedCampus) {
+                Text("金盆岭校区").tag(Campus.jinpenling)
+                Text("云塘校区").tag(Campus.yuntang)
+            }
+            .pickerStyle(.segmented)
+            .padding(.horizontal)
+            .padding(.top)
+
+            GeometryReader { geo in
+                Image(mapImageName)
+                    .resizable()
+                    .scaledToFit()
+                    .zoomable(minZoomScale: 1, doubleTapZoomScale: 3, outOfBoundsColor: .clear)
+                    .frame(width: geo.size.width,
+                           height: geo.size.height)
+                    .clipped()
+                    .contentShape(Rectangle())
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        .navigationTitle("校园地图")
     }
 }
 
 #Preview {
-    CampusMapView()
+    NavigationStack {
+        CampusMapView()
+    }
 }
