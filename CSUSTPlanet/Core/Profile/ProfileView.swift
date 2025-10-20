17bcad2f4a2dc536daac54e6b2e8053c853d5cc9
zHElEARN <personal@zhelearn.com>
2025-10-20 15:57:34 +0800

    feat(我的): 优化页面Section部分顺序

--- CSUSTPlanet/Core/Profile/ProfileView.swift
+++ CSUSTPlanet/Core/Profile/ProfileView.swift
@@ -41,6 +41,36 @@ struct ProfileView: View {
                         }
                     }
 
+                    if authManager.isEducationLoggingIn {
+                        Button(action: authManager.loginToEducation) {
+                            HStack {
+                                ProgressView().padding(.horizontal, 6).id(authManager.eduLoginID)
+                                Text("正在登录教务系统 (点击重试)")
+                            }
+                        }
+                    } else {
+                        Button(action: authManager.loginToEducation) {
+                            ColoredLabel(title: "重新登录教务系统", iconName: "graduationcap", color: .orange)
+                        }
+                        .buttonStyle(.plain)
+                        .disabled(!authManager.isLoggedIn)
+                    }
+
+                    if authManager.isMoocLoggingIn {
+                        Button(action: authManager.loginToMooc) {
+                            HStack {
+                                ProgressView().padding(.horizontal, 6).id(authManager.moocLoginID)
+                                Text("正在登录网络课程中心 (点击重试)")
+                            }
+                        }
+                    } else {
+                        Button(action: authManager.loginToMooc) {
+                            ColoredLabel(title: "重新登录网络课程中心", iconName: "book.closed", color: .mint)
+                        }
+                        .buttonStyle(.plain)
+                        .disabled(!authManager.isLoggedIn)
+                    }
+
                     Button(action: {
                         Task {
                             try await authManager.logout()
@@ -71,6 +101,21 @@ struct ProfileView: View {
                 }
             }
 
+            Section(header: Text("设置"), footer: Text("实时活动/灵动岛将会显示：上课前20分钟、上课中和下课后5分钟的课程状态")) {
+                Picker(selection: $globalVars.appearance) {
+                    Text("浅色模式").tag("light")
+                    Text("深色模式").tag("dark")
+                    Text("跟随系统").tag("system")
+                } label: {
+                    ColoredLabel(title: "外观主题", iconName: "paintbrush", color: .purple)
+                }
+
+                Toggle(isOn: $globalVars.isLiveActivityEnabled) {
+                    ColoredLabel(title: "启用实时活动/灵动岛", iconName: "bolt.circle", color: .yellow)
+                }
+                .onChange(of: globalVars.isLiveActivityEnabled, { _, _ in ActivityManager.shared.autoUpdateActivity() })
+            }
+
             Section(header: Text("帮助与支持")) {
                 NavigationLink {
                     AboutView()
@@ -88,51 +133,6 @@ struct ProfileView: View {
                     UserAgreementView()
                 } label: {
                     ColoredLabel(title: "用户协议", iconName: "doc.text", color: .indigo)
-                }
-            }
-
-            Section(header: Text("其他")) {
-                Picker(selection: $globalVars.appearance) {
-                    Text("浅色模式").tag("light")
-                    Text("深色模式").tag("dark")
-                    Text("跟随系统").tag("system")
-                } label: {
-                    ColoredLabel(title: "外观主题", iconName: "paintbrush", color: .purple)
-                }
-
-                Toggle(isOn: $globalVars.isLiveActivityEnabled) {
-                    ColoredLabel(title: "启用实时活动/灵动岛", iconName: "bolt.circle", color: .yellow)
-                }
-                .onChange(of: globalVars.isLiveActivityEnabled, { _, _ in ActivityManager.shared.autoUpdateActivity() })
-
-                if authManager.isEducationLoggingIn {
-                    Button(action: authManager.loginToEducation) {
-                        HStack {
-                            ProgressView().padding(.horizontal, 6).id(authManager.eduLoginID)
-                            Text("正在登录教务系统 (点击重试)")
-                        }
-                    }
-                } else {
-                    Button(action: authManager.loginToEducation) {
-                        ColoredLabel(title: "重新登录教务系统", iconName: "graduationcap", color: .orange)
-                    }
-                    .buttonStyle(.plain)
-                    .disabled(!authManager.isLoggedIn)
-                }
-
-                if authManager.isMoocLoggingIn {
-                    Button(action: authManager.loginToMooc) {
-                        HStack {
-                            ProgressView().padding(.horizontal, 6).id(authManager.moocLoginID)
-                            Text("正在登录网络课程中心 (点击重试)")
-                        }
-                    }
-                } else {
-                    Button(action: authManager.loginToMooc) {
-                        ColoredLabel(title: "重新登录网络课程中心", iconName: "book.closed", color: .mint)
-                    }
-                    .buttonStyle(.plain)
-                    .disabled(!authManager.isLoggedIn)
                 }
             }
         }
